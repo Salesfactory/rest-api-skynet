066c0aaa5c2fb9e4b3ccd5f8774667d2137ef95a
pedrolabrador <pedrolabrador3@outlook.com>
2023-06-22 22:22:32 -0400

    SKN-38 install and setup of BigQuery

--- index.js
+++ index.js
@@ -5,11 +5,38 @@ const bodyParser = require("body-parser");
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
+// BigQuery
+const { BigQuery } = require('@google-cloud/bigquery');
+
 app.use(bodyParser.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
   res.send('Hello!');
+});
+
+// Create a new dataset for testing, this might be removed later
+// move routes to a new file
+app.get("/create-test-dataset", (req, res) => {
+  try {
+    const datasetName = req.query.datasetName;
+    async function createDataset() {
+      // Creates a client
+      const bigqueryClient = new BigQuery();
+  
+      // Create the dataset
+      const [dataset] = await bigqueryClient.createDataset(datasetName);
+      console.log(`Dataset ${dataset.id} created.`);
+      res.send(`Dataset ${dataset.id} created.`);
+    }
+    if (datasetName) {
+      createDataset();
+    } else {
+      res.send("Please provide dataset name");
+    }
+  } catch (error) {
+    res.send(error.message);
+  }
 });
 
 app.listen(port, (err) => {
