61faa92d8f8069cdf0cf8bdc50ca5d188f5625ec
pedrolabrador <pedrolabrador3@outlook.com>
2023-07-05 17:35:37 -0400

    SKN-88 connect project envs to google query

---
+++ src/config/bigquery.js
@@ -0,0 +1,28 @@
+const { BigQuery } = require('@google-cloud/bigquery');
+
+const projectId = process.env.project_id;
+
+const keyFileContent = `{
+    "type": "${process.env.type}",
+    "project_id": "${process.env.project_id}",
+    "private_key_id": "${process.env.private_key_id}",
+    "private_key": "${process.env.private_key}",
+    "client_email": "${process.env.client_email}",
+    "client_id": "${process.env.client_id}",
+    "auth_uri": "${process.env.auth_uri}",
+    "token_uri": "${process.env.token_uri}",
+    "auth_provider_x509_cert_url": "${process.env.auth_provider_x509_cert_url}",
+    "client_x509_cert_url": "${process.env.client_x509_cert_url}",
+    "universe_domain": "${process.env.universe_domain}"
+}`;
+
+const credentials = JSON.parse(keyFileContent);
+
+const options = {
+    projectId,
+    credentials,
+};
+
+const bigqueryClient = new BigQuery(options);
+
+module.exports = bigqueryClient;
