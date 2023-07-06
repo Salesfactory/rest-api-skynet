a3b5996c9fce191cf36a1726659cb8cf6ffdf5e3
pedrolabrador <pedrolabrador3@outlook.com>
2023-07-06 18:01:43 -0400

    updated envvars

--- src/config/bigquery.js
+++ src/config/bigquery.js
@@ -3,17 +3,17 @@ const { BigQuery } = require('@google-cloud/bigquery');
 const projectId = process.env.project_id;
 
 const keyFileContent = `{
-    "type": "${process.env.type}",
-    "project_id": "${process.env.project_id}",
-    "private_key_id": "${process.env.private_key_id}",
-    "private_key": "${process.env.private_key}",
-    "client_email": "${process.env.client_email}",
-    "client_id": "${process.env.client_id}",
-    "auth_uri": "${process.env.auth_uri}",
-    "token_uri": "${process.env.token_uri}",
-    "auth_provider_x509_cert_url": "${process.env.auth_provider_x509_cert_url}",
-    "client_x509_cert_url": "${process.env.client_x509_cert_url}",
-    "universe_domain": "${process.env.universe_domain}"
+    "type": "${process.env.BQ_TYPE}",
+    "project_id": "${process.env.BQ_PROJECT_ID}",
+    "private_key_id": "${process.env.BQ_PRIVATE_KEY_ID}",
+    "private_key": "${process.env.BQ_PRIVATE_KEY}",
+    "client_email": "${process.env.BQ_CLIENT_EMAIL}",
+    "client_id": "${process.env.BQ_CLIENT_ID}",
+    "auth_uri": "${process.env.BQ_AUTH_URI}",
+    "token_uri": "${process.env.BQ_TOKEN_URI}",
+    "auth_provider_x509_cert_url": "${process.env.BQ_AUTH_PROVIDER_X509_CERT_URL}",
+    "client_x509_cert_url": "${process.env.BQ_CLIENT_X509_CERT_URL}",
+    "universe_domain": "${process.env.BQ_UNIVERSE_DOMAIN}"
 }`;
 
 const credentials = JSON.parse(keyFileContent);
