e3193936334cd96d5ff03b3e45977b9b01997099
Manuel Castro <maacaro@gmail.com>
2023-08-05 18:22:30 -0400

    SKN-156 create Get /clients/:clientId endpoint

--- src/controllers/client.js
+++ src/controllers/client.js
@@ -11,7 +11,27 @@ const getClients = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+const getClient = async (req, res) => {
+    const { clientId } = req.params;
+    try {
+        const client = await Client.findOne({
+            where: { id: clientId },
+        });
 
+        if (!client) {
+            return res.status(404).json({
+                message: `Client not found`,
+            });
+        }
+        return res.status(200).json({
+            message: 'Client retrieved successfully',
+            data: client,
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 module.exports = {
     getClients,
+    getClient,
 };
