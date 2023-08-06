abc8a15b846de46b05876ba5d60e89d0c2452769
Manuel Castro <maacaro@gmail.com>
2023-08-06 07:16:40 -0400

    SKN-156 create Get /clients/:clientId endpoint (#42)

--- src/routes/clients.js
+++ src/routes/clients.js
@@ -4,21 +4,55 @@ const { clientController, campaignController } = require('../controllers');
 
 // client routes
 router.get('/', clientController.getClients);
+router.get('/:clientId', clientController.getClient);
 // non orchestrated campaign advertisements
-router.get('/:id/advertisements', campaignController.getClientCampaignAdvertisements);
+router.get(
+    '/:id/advertisements',
+    campaignController.getClientCampaignAdvertisements
+);
 // marketing campaigns routes
-router.get('/:id/marketingcampaign', campaignController.getMarketingCampaignsByClient);
-router.get('/:id/marketingcampaign/:cid', campaignController.getMarketingCampaignsById);
-router.post('/:id/marketingcampaign', campaignController.createMarketingCampaign);
-router.put('/:id/marketingcampaign/:cid', campaignController.updateMarketingCampaign);
-router.delete('/:id/marketingcampaign/:cid', campaignController.deleteMarketingCampaign);
+router.get(
+    '/:id/marketingcampaign',
+    campaignController.getMarketingCampaignsByClient
+);
+router.get(
+    '/:id/marketingcampaign/:cid',
+    campaignController.getMarketingCampaignsById
+);
+router.post(
+    '/:id/marketingcampaign',
+    campaignController.createMarketingCampaign
+);
+router.put(
+    '/:id/marketingcampaign/:cid',
+    campaignController.updateMarketingCampaign
+);
+router.delete(
+    '/:id/marketingcampaign/:cid',
+    campaignController.deleteMarketingCampaign
+);
 // marketing campaigns advertisements routes
-router.get('/:id/marketingcampaign/:cid/campaigns', campaignController.getCampaignsByGroup);
-router.get('/:id/marketingcampaign/:cid/campaigns/:caid', campaignController.getCampaignsById);
-router.put('/:id/marketingcampaign/:cid/campaigns/:caid/goals', campaignController.updateCampaignGoals);
+router.get(
+    '/:id/marketingcampaign/:cid/campaigns',
+    campaignController.getCampaignsByGroup
+);
+router.get(
+    '/:id/marketingcampaign/:cid/campaigns/:caid',
+    campaignController.getCampaignsById
+);
+router.put(
+    '/:id/marketingcampaign/:cid/campaigns/:caid/goals',
+    campaignController.updateCampaignGoals
+);
 // pause campaign from group
-router.put('/:id/marketingcampaign/:cid/campaigns/:caid/pause', campaignController.pauseCampaign);
+router.put(
+    '/:id/marketingcampaign/:cid/campaigns/:caid/pause',
+    campaignController.pauseCampaign
+);
 // delete campaign from group
-router.delete('/:id/marketingcampaign/:cid/campaigns/:caid', campaignController.deleteCampaign);
+router.delete(
+    '/:id/marketingcampaign/:cid/campaigns/:caid',
+    campaignController.deleteCampaign
+);
 
 module.exports = router;
