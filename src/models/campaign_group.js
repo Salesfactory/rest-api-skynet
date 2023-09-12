cf1bc2f8a198754b24434ee93d2dc8d1c6831d35
pedrolabrador <pedrolabrador3@outlook.com>
2023-09-12 15:25:06 -0400

    default value

--- src/models/campaign_group.js
+++ src/models/campaign_group.js
@@ -77,6 +77,7 @@ module.exports = (sequelize, DataTypes) => {
             status: {
                 type: DataTypes.STRING,
                 allowNull: false,
+                defaultValue: 'N/S',
             },
         },
         {
