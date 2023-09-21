fed17411cdbfc8eda3c6151b6521eb09feed0bed
Manuel Castro <maacaro@gmail.com>
2023-09-20 20:08:22 -0400

    Skn 173 cronjob for pacing (#79)
    * SKN-173 cronjob for budget pacing
    
    * Update cronjobs.js
    
    * SKN-173 abstrat spending into a function
    
    * SKN-173 refactor add functions for readability
    
    * SKN-173 add test to metric functions
    
    * SKN-173 tests
    
    * SKN-173 fix date format
    
    * SKN-173 moving tests to another file
    
    * SKN-173 fix cronjob
    
    ---------
    
    Co-authored-by: pedrolabrador <pedrolabrador3@outlook.com>
    Co-authored-by: Pedro Labrador <25873364+PedroLabrador@users.noreply.github.com>

--- src/cronjobs.js
+++ src/cronjobs.js
@@ -1,6 +1,7 @@
 const cron = require('node-cron');
-const { Channel } = require('./models');
+const { Budget, CampaignGroup, Channel, Pacing } = require('./models');
 const { channelController } = require('./controllers');
+const { computeAndStoreMetrics } = require('./utils/bq_spend');
 
 const formattedTime = time => {
     return (
@@ -23,31 +24,113 @@ const start = () => {
 
         // every day at 0 hours 0 minutes check if there are new channels to be inserted in the database
         cron.schedule('0 0 * * *', async () => {
-            logMessage('Starting daily check for new channels');
-            const bqChannels =
-                await channelController.getProtectedBigqueryChannels();
-            const channels = await Channel.findAll();
-
-            // iterate over all channels from bigquery and check if they are already in the database
-            for (const bqChannel of bqChannels) {
-                const channel = channels.find(
-                    channel => channel.name === bqChannel.channel
-                );
-
-                // if channel is not in the database, insert it
-                if (!channel) {
-                    logMessage(
-                        `Channel ${bqChannel.channel} not found in the database. Inserting it now.`
-                    );
-                    await Channel.create({
-                        name: bqChannel.channel,
-                    });
-                }
+            try {
+                await checkAndInsertNewChannels();
+            } catch (error) {
+                logMessage('Error while checking for new channels: ' + error);
             }
+        });
 
-            logMessage('Finished daily check for new channels');
+        // update all budget metrics every day at 2 hours 0 minutes
+        cron.schedule('0 2 * * *', async () => {
+            try {
+                await updateBudgetMetrics();
+            } catch (error) {
+                console.log(error);
+                logMessage('Error while updating budget metrics: ' + error);
+            }
         });
     }
 };
+
+async function checkAndInsertNewChannels() {
+    logMessage('Starting daily check for new channels');
+    const bqChannels = await channelController.getProtectedBigqueryChannels();
+
+    for (const bqChannel of bqChannels) {
+        const channelExists = await Channel.findOne({
+            where: { name: bqChannel.channel },
+        });
+
+        if (!channelExists) {
+            logMessage(
+                `Channel ${bqChannel.channel} not found in the database. Inserting it now.`
+            );
+            await Channel.create({
+                name: bqChannel.channel,
+            });
+        }
+    }
+    logMessage('Finished daily check for new channels');
+}
+
+async function updateBudgetMetrics() {
+    logMessage('Starting daily budget metrics update');
+
+    const campaigns = await fetchCampaignsWithBudgets();
+    const currentDate = new Date();
+
+    for (const campaign of campaigns) {
+        logMessage('Calculating metrics for campaign: ' + campaign.id);
+        const { periods, allocations } = await computeAndStoreMetrics({
+            campaign,
+            currentDate,
+        });
+        logMessage("Updating campaign's metrics in the database");
+        await updateOrInsertPacingMetrics({
+            campaign,
+            periods,
+            allocations,
+        });
+    }
+
+    logMessage('Finished daily budget metrics update');
+}
+
+async function fetchCampaignsWithBudgets() {
+    return CampaignGroup.findAll(
+        {
+            where: { deleted: false },
+            include: [
+                {
+                    model: Budget,
+                    as: 'budgets',
+                    limit: 1,
+                    order: [['updatedAt', 'DESC']],
+                    attributes: ['periods', 'allocations'],
+                },
+            ],
+        },
+        { raw: true, plain: true }
+    );
+}
+
+async function updateOrInsertPacingMetrics({
+    campaign,
+    periods,
+    allocationsCopy,
+}) {
+    const campaignPacing = await Pacing.findOne({
+        where: { campaign_group_id: campaign.id },
+    });
+
+    if (campaignPacing) {
+        await Pacing.update(
+            {
+                periods: periods,
+                allocations: allocationsCopy,
+            },
+            {
+                where: { campaign_group_id: campaign.id },
+            }
+        );
+    } else {
+        await Pacing.create({
+            campaign_group_id: campaign.id,
+            periods: periods,
+            allocations: allocationsCopy,
+        });
+    }
+}
 
 module.exports = { start };
