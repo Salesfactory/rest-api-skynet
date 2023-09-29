6f33377a8390b1057d9ddee08f9fddcec58a5b2e
pedrolabrador <pedrolabrador3@outlook.com>
2023-09-29 14:30:21 -0400

    SKN email err handle

--- src/utils/email.js
+++ src/utils/email.js
@@ -37,7 +37,7 @@ const send = ({ to, subject, message, html = null }) => {
         try {
             transporter.verify(function (error, success) {
                 if (error) {
-                    return reject(err);
+                    return reject(error);
                 } else {
                     transporter.sendMail(emailOption, (err, info) => {
                         if (!err) {
