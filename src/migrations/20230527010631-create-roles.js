0393e58082a29596f476101858e1d98c07db215a
Pedro Labrador <25873364+PedroLabrador@users.noreply.github.com>
2023-10-27 12:05:01 -0400

    SKN-374 design tables to store role permissions (#121)
    * SKN-374 design tables to store role permissions
    
    * SKN-374 adding models
    
    * SKN-374 adjust default role
    
    * SKN-374 corrected roles and permissions
    
    * SKN-374 added aliases to models

---
+++ src/migrations/20230527010631-create-roles.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('roles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      }
+    });
+
+    // Seed default roles
+    await queryInterface.bulkInsert('roles', [
+      { name: 'Admin', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'DM', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Super', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Analyst', createdAt: new Date(), updatedAt: new Date() },
+    ]);
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('roles');
+  }
+};
