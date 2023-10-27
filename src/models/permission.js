17ce9b312a480f544ba1bbf9c3b2ac586bc2c47d
pedrolabrador <pedrolabrador3@outlook.com>
2023-10-27 11:55:09 -0400

    SKN-374 added aliases to models

--- src/models/permission.js
+++ src/models/permission.js
@@ -7,6 +7,7 @@ module.exports = (sequelize, DataTypes) => {
                 through: 'rolePermissions',
                 foreignKey: 'permissionId',
                 otherKey: 'roleId',
+                as: 'roles',
             });
         }
     }
--- src/models/role.js
+++ src/models/role.js
@@ -7,6 +7,7 @@ module.exports = (sequelize, DataTypes) => {
                 through: 'rolePermissions',
                 foreignKey: 'roleId',
                 otherKey: 'permissionId',
+                as: 'permissions',
             });
         }
     }
