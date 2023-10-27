d2bf43052ee2b389a9b2b4890688fb744af9237d
Manuel Castro <maacaro@gmail.com>
2023-10-27 15:18:01 -0400

    SKN 413 create services for creating campaigns (#122)
    * SKN-413 create validation and config functions
    
    * SKN - 413 get data for sponsored products
    
    * SKN - 413 refactor getSponsoredProductsCreateData
    
    * SKN - 413 get sponsored Brands data
    
    * SKN -413 refactor fnction getSponsoredBrandsCreateData
    
    * SKN-413  get sponsored display data
    
    * SKN - 413 create campaigns
    
    * SKN -413 create helper function for POST create campaign
    
    ---------
    
    Co-authored-by: Pedro Labrador <25873364+PedroLabrador@users.noreply.github.com>

--- tests/campaigns.spec.js
+++ tests/campaigns.spec.js
@@ -2,6 +2,46 @@ const supertest = require('supertest');
 const makeApp = require('../src/app');
 const { Budget, Campaign, CampaignGroup, Client } = require('../src/models');
 const { getUser } = require('../src/utils');
+const { createAmazonCampaign } = require('../src/utils/campaign-controller');
+// Mocked utility functions
+jest.mock('../src/utils/allocations', () => ({
+    validateCredentials: jest.fn(),
+    validateCampaignsArray: jest.fn(),
+    getConfig: jest.fn(),
+    createCampaigns: jest.fn(),
+}));
+const {
+    validateCredentials,
+    validateCampaignsArray,
+    getConfig,
+    createCampaigns,
+} = require('../src/utils/allocations');
+const mockData = {
+    campaigns: {
+        'Sponsored Products': [
+            {
+                name: 'SP Campaign 1',
+                startDate: '2023-01-01',
+                endDate: '2023-01-10',
+                budget: 100,
+            },
+        ],
+        'Sponsored Brands': [
+            {
+                name: 'SB Campaign 1',
+                startDate: '2023-01-15',
+                endDate: '2023-01-25',
+                budget: 200,
+            },
+        ],
+    },
+    state: 'ACTIVE',
+    profileId: '12345',
+    access: {
+        CLIENT_ID: 'your-client-id',
+        ACCESS_TOKEN: 'your-access-token',
+    },
+};
 
 jest.mock('../src/models', () => ({
     User: {
@@ -738,4 +778,69 @@ describe('Campaign Endpoints Test', () => {
             expect(response.body.message).toBe('Error');
         });
     });
+});
+
+describe('createAmazonCampaign', () => {
+    it('should create Amazon campaigns successfully', async () => {
+        // Mock utility function behaviors
+        validateCredentials.mockImplementation(() => {});
+        validateCampaignsArray.mockImplementation(() => {});
+        getConfig.mockImplementation(() => ({
+            /* mock config object */
+        }));
+        createCampaigns.mockImplementation(() => ({
+            errors: [],
+            successes: [{ y: 'success' }],
+        }));
+
+        // Call the function
+        const result = await createAmazonCampaign(mockData);
+
+        // Assertions
+        expect(result.message).toBe('Amazon campaign creation complete');
+        expect(result.success).toEqual([{ y: 'success' }, { y: 'success' }]);
+        expect(result.error).toEqual([]);
+    });
+
+    it('should handle errors during campaign creation', async () => {
+        // Mock utility function behaviors
+        validateCredentials.mockImplementation(() => {});
+        validateCampaignsArray.mockImplementation(() => {});
+        getConfig.mockImplementation(() => ({}));
+        createCampaigns.mockImplementation(() => ({
+            errors: [{ foo: 'bar' }],
+            successes: [],
+        }));
+
+        // Call the function
+        const result = await createAmazonCampaign(mockData);
+
+        // Assertions
+        expect(result.message).toBe('Amazon campaign creation complete');
+        expect(result.success).toEqual([]);
+        expect(result.error).toEqual([{ foo: 'bar' }, { foo: 'bar' }]);
+    });
+
+    it('should handle validation errors', async () => {
+        // Mock utility function behaviors to throw validation errors
+        validateCredentials.mockImplementation(() => {
+            throw new Error('Access token or client ID is missing');
+        });
+        validateCampaignsArray.mockImplementation(() => {
+            throw new Error('Campaigns must be an array');
+        });
+
+        // Call the function
+        const result = await createAmazonCampaign(mockData);
+
+        // Assertions
+        expect(result.message).toBe('Amazon campaign creation failed');
+        expect(result.success).toEqual([]);
+        expect(result.error).toEqual({
+            code: 500,
+            message: 'Access token or client ID is missing', // Check the actual error message
+        });
+    });
+
+    // Add more test cases to cover different scenarios and edge cases
 });
