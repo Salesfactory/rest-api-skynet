9d6f0eef637b1b0a1a0a64b3c65fa1a5f7cd86c9
Manuel Castro <maacaro@gmail.com>
2023-11-30 19:31:55 -0400

    SKN 487 replace redis queue by postgress

--- src/services/queue/queue.js
+++ src/services/queue/queue.js
@@ -1,49 +1,39 @@
-const { Queue, Worker } = require('bullmq');
-const IORedis = require('ioredis');
-
-// Configuration for connecting to ElastiCache Redis
-
-function createQueue() {
-    const redisConfig = {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT,
-        password: process.env.REDIS_PASSWORD || undefined, // If your ElastiCache Redis has a password
-        maxRetriesPerRequest: null,
-    };
-    console.log(JSON.stringify(redisConfig, null, 2));
-    const connection = new IORedis(redisConfig);
-    const amzQueue = new Queue('AmzQueue', { connection });
-
+function createQueue(jobs) {
     return {
         addJobToQueue: async jobData => {
             try {
-                const job = await amzQueue.add('myJob', jobData);
+                const job = await jobs.create({
+                    data: jobData,
+                });
                 console.log(`Job added with ID: ${job.id}`);
                 return job.id;
-            } catch (err) {
-                console.error('Error adding job to the queue:', err);
-                throw new Error(err);
+            } catch (error) {
+                console.error('Error adding job to the queue:', error);
+                throw new Error(error);
             }
         },
         startProcessingtJobs: async jobProcessingLogic => {
-            const worker = new Worker(
-                'AmzQueue',
-                async job => {
-                    console.log(`Processing job ${job.id} with data`, job.data);
-                    await jobProcessingLogic(job);
-                },
-                { connection }
-            );
+            const job = await jobs.findOne({ where: { status: 'pending' } });
 
-            worker.on('completed', job => {
-                console.log(`Job ${job.id} has completed!`);
-            });
+            if (job) {
+                await job.update({ status: 'processing' });
 
-            worker.on('failed', (job, err) => {
-                console.log(
-                    `Job ${job.id} has failed with error ${err.message}`
-                );
+                jobProcessingLogic(job);
+                console.log(`Processing job: ${job.id}`);
+
+                // Update status to 'completed'
+                await job.update({
+                    status: 'completed',
+                    processedAt: new Date(),
+                });
+            }
+        },
+        getCompletedJobs: async () => {
+            const completedJobs = await jobs.findAll({
+                where: { status: 'completed' },
+                attributes: ['id', 'processedAt'],
             });
+            return completedJobs;
         },
     };
 }
